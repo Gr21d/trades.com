e0a096b875fd9f289b148c239ddadf6e6f964e4d
gargipande04 <162762239+gargipande04@users.noreply.github.com>
2024-04-05 18:56:52 +0100

    Update page4.js

--- pages/tutorials/portfoliopagetutorial/page4.js
+++ pages/tutorials/portfoliopagetutorial/page4.js
@@ -1,15 +1,15 @@
 // Page4.js
 import React from 'react';
 import Link from 'next/link';
-import '../../../styles/TutorialBox.css';
+import '/tutorials/portfoliopagetutorial/TutorialBox.css'; 
 
 const Page4 = () => {
   return (
     <div className="tutorial-box">
       <h3>Tips for a Smooth Transaction</h3>
       <p>Always double-check the current price and the amount you wish to sell. Ensure that the destination number is accurate to avoid misdirecting funds.</p>
-      <Link href="/tutorials/profilepagetutorial/page3">
-        <a className="back-button">Back</a>
+      <Link href="/tutorials/portfoliopagetutorial/page3">
+        <span className="back-button">Back</span>
       </Link>
     </div>
   );
