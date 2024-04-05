ed33f39c4b895dd2bb0a231a27770c4a2406b071
gargipande04 <162762239+gargipande04@users.noreply.github.com>
2024-04-05 18:54:13 +0100

    Update page1.js

--- pages/tutorials/portfoliopagetutorial/page1.js
+++ pages/tutorials/portfoliopagetutorial/page1.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
-import '../../../styles/TutorialBox.css'; 
+import '/tutorials/portfoliopagetutorial/TutorialBox.css'; 
 
 const Page1 = () => {
   return (
     <div className="tutorial-box">
       <h2>User Guide: Cryptocurrency Portfolio Page with Selling Feature</h2>
-      <Link href="/tutorials/profilepagetutorial/page2">
-        <a className="next-button">Next</a>
+       <Link href="/tutorials/portfoliopagetutorial/page2" passHref>
+        <span className="next-button">Next</span>
       </Link>
     </div>
   );
