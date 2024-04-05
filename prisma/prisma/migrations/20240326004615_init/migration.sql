291fc32fe65d5ee4e9fac759b62abf0f7564d86d
OmarEltanbdawy <129180252+OmarEltanbdawy@users.noreply.github.com>
2024-04-05 16:47:50 +0100

    Delete prisma directory

--- prisma/prisma/migrations/20240326004615_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "content" TEXT,
-    "published" BOOLEAN NOT NULL DEFAULT false,
-    "authorId" INTEGER
-);
